dd62bd965adfa5b5eec9bcd9b404e0709c0ac28e
Vladimir Abrosimov <vladimir.abrosimovk@gmail.com>
2023-05-22 15:52:26 +0700

    Creating project

---
+++ app/src/main/java/com/example/primitivetodolist/AddNoteActivity.java
@@ -0,0 +1,89 @@
+package com.example.primitivetodolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    private EditText editTextNote;
+    private RadioButton radioButtonLow;
+    private RadioButton radioButtonMedium;
+    private RadioButton radioButtonHigh;
+    private Button buttonSave;
+
+    private NoteDatabase noteDatabase;
+
+    private Handler handler = new Handler(Looper.getMainLooper());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        noteDatabase = NoteDatabase.getInstance(getApplication());
+        initViews();
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveNote();
+
+            }
+        });
+    }
+
+    private void initViews() {
+        editTextNote = findViewById(R.id.editTextNote);
+        radioButtonLow = findViewById(R.id.radioButtonLow);
+        radioButtonMedium = findViewById(R.id.radioButtonMedium);
+        radioButtonHigh = findViewById(R.id.radioButtonHigh);
+        buttonSave = findViewById(R.id.buttonSave);
+    }
+
+    private void saveNote() {
+        String text = editTextNote.getText().toString().trim();
+        int priority = getPriority();
+        Note note = new Note( text, priority);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                noteDatabase.notesDao().add(note);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        finish();
+                    }
+                });
+            }
+        });
+        thread.start();
+
+
+
+    }
+
+    private int getPriority() {
+        int priority;
+        if (radioButtonLow.isChecked()) {
+            priority = 0;
+        } else if (radioButtonMedium.isChecked()) {
+            priority = 1;
+        } else {
+            priority = 2;
+        }
+        return priority;
+    }
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, AddNoteActivity.class);
+    }
+
+}
---
+++ app/src/main/java/com/example/primitivetodolist/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.primitivetodolist;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewNotes;
+    private FloatingActionButton buttonAddNote;
+    private NotesAdapter notesAdapter;
+
+    private NoteDatabase noteDatabase;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        noteDatabase = NoteDatabase.getInstance(getApplication());
+        initViews();
+
+        notesAdapter = new NotesAdapter();
+        notesAdapter.setOnNoteClickListener(new NotesAdapter.OnNoteClickListener() {
+            @Override
+            public void onNoteClock(Note note) {
+            }
+        });
+        recyclerViewNotes.setAdapter(notesAdapter);
+
+        noteDatabase.notesDao().getNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                notesAdapter.setNotes(notes);
+            }
+        });
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(
+                new ItemTouchHelper.SimpleCallback(
+                        0,
+                        ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT
+                ) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView,
+                                  @NonNull RecyclerView.ViewHolder viewHolder,
+                                  @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                Note note = notesAdapter.getNotes().get(position);
+
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        noteDatabase.notesDao().remove(note.getId());
+                    }
+                });
+                thread.start();
+
+            }
+        });
+        itemTouchHelper.attachToRecyclerView(recyclerViewNotes);
+
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = AddNoteActivity.newIntent(MainActivity.this);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+    private void initViews() {
+        recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
+        buttonAddNote = findViewById(R.id.buttonAddNote);
+    }
+
+
+    }
